667d9df9c225bff4ab9f6315b85ac0a12ce12816
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-04-14 15:15:52 -0400

    Saving Data to the Database

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -24,7 +24,7 @@ namespace Tasker.Pages
 
         public IActionResult OnPost()
         {
-            _context.Add(NewTask);
+            _context.Tasks.Add(NewTask);
             _context.SaveChanges();
             return RedirectToPage("Index");
         }
